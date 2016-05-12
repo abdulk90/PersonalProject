766adab01ca512ab9b5db9cf050c7b5f5bf0b250
abdulk90 <abdul.kayyali@gmail.com>
2016-05-12 15:36:49 -0400

    added sport descriptions column and actual descriptions of sports

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512014748) do
+ActiveRecord::Schema.define(version: 20160512184839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,8 +78,9 @@ ActiveRecord::Schema.define(version: 20160512014748) do
 
   create_table "sport_types", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "teams", force: :cascade do |t|
